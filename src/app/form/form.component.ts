3ac54317da1bdd8846226024a9d748e6c070ae4e
leen <leenodeh287@gmail.com>
2023-08-28 22:15:43 +0300

    add task2 & update task1

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  name:String='';
+
+  onclick(){
+    this.name='';
+  }
+
+  submit(event:Event){
+    this.name=(event.target as HTMLInputElement).value;
+  }
+}
